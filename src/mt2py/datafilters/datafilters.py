a3c4c5f6bc7f37a819e95ccbd6f40dcc0c0e022d
Rory Spencer <rory.spencer@ukaea.uk>
2025-05-08 06:57:41 +0100

    Adding interpolations

--- src/mt2py/datafilters/datafilters.py
+++ src/mt2py/datafilters/datafilters.py
@@ -13,6 +13,7 @@ from mt2py.spatialdata.importdicedata import simdata_dice_to_spatialdata
 from mt2py.spatialdata.tensorfield import rank_two_field
 from mt2py.spatialdata.tensorfield import vector_field
 from mt2py.spatialdata.tensorfield import scalar_field
+from mt2py.spatialdata.tensorfield import symmetric_rank_two_field
 
 from scipy.spatial import cKDTree as KDTree
 from scipy.spatial import Delaunay
@@ -164,6 +165,69 @@ class FastFilter(DataFilterBase):
         u_int = np.reshape(u_int,(-1,fe_data.n_steps))
         v_int = np.reshape(v_int,(-1,fe_data.n_steps))
         return result, u_int, v_int
+        
+    @staticmethod
+    def interpolate_to_grid_generic(fe_data : SpatialData,spacing : float, exclude_limit: float,field: str):
+        """Interpolate the FE data onto a regular grid with spacing.
+
+        Args:
+            fe_data (SpatialData): FE Data.
+            spacing (float): Spacing for the regular grid on which the data will be interpolated
+
+        Returns:
+            _type_: _description_
+        """
+
+        bounds = fe_data.mesh_data.bounds
+        # Create regular grid to interpolate to
+        xr = np.linspace(bounds[0],bounds[1],int((bounds[1]-bounds[0])/spacing))
+        yr = np.linspace(bounds[2],bounds[3],int((bounds[3]-bounds[2])/spacing))
+        zr = bounds[5]
+        x,y = np.meshgrid(xr,yr,indexing='ij')
+        # Add Nans to the array for outline the edges of the specimen
+        
+        if exclude_limit >0:
+            xp,yp = FastFilter.excluding_mesh(fe_data.mesh_data.points[:,0], fe_data.mesh_data.points[:,1], nx=exclude_limit, ny=exclude_limit)
+            zp = np.nan + np.zeros_like(xp)
+            points = np.transpose(np.vstack((np.r_[fe_data.mesh_data.points[:,0],xp], np.r_[fe_data.mesh_data.points[:,1],yp])))
+        
+            tri = Delaunay(points)
+            n_comp = fe_data.data_fields[field].data.shape[1]
+            dat_int = np.empty((x.shape[0],x.shape[1],fe_data.n_steps,n_comp))
+            for i in range(fe_data.n_steps):
+                for j in range(n_comp):
+                    zd = fe_data.data_fields[field].data[:,j,i]
+                    dat_int[:,:,i,j] = interpolate.LinearNDInterpolator(tri,np.r_[zd,zp])(x,y)
+        
+        else: # Don't use excluding mesh approach
+            points = np.transpose(np.vstack((np.r_[fe_data.mesh_data.points[:,0]], np.r_[fe_data.mesh_data.points[:,1]])))
+        
+            tri = Delaunay(points)
+            u_int = np.empty((x.shape[0],x.shape[1],fe_data.n_steps))
+            v_int = np.empty((x.shape[0],x.shape[1],fe_data.n_steps))
+            for i in range(fe_data.n_steps):
+                zu = fe_data.data_fields['displacement'].data[:,0,i]
+                zv = fe_data.data_fields['displacement'].data[:,1,i]
+                u_int[:,:,i] = interpolate.LinearNDInterpolator(tri,np.r_[zu])(x,y)
+                v_int[:,:,i] = interpolate.LinearNDInterpolator(tri,np.r_[zv])(x,y)
+ 
+
+        # Create pyvista mesh 
+        x,y = np.meshgrid(xr,yr,indexing='ij')
+
+        dat_int = np.reshape(dat_int,(x.shape[0],x.shape[1],fe_data.n_steps,n_comp))
+        nan_mask = np.isnan(dat_int[...,0,0])
+        print(dat_int.shape)
+        print(nan_mask.shape)
+        # Create pyvista mesh 
+        #x,y,z = np.meshgrid(xr,yr,zr)
+        #print(nan_mask.shape)
+        #print(x.shape)
+        #nan_mask=np.reshape(nan_mask,x.shape)
+        x[nan_mask] = np.nan
+        y[nan_mask] = np.nan
+        return x,y,dat_int
+    
     
     @staticmethod
     def interpolate_to_mesh(fe_data : SpatialData, dic_data_mesh: pv.UnstructuredGrid, exclude_limit: float):
@@ -270,6 +334,8 @@ class FastFilter(DataFilterBase):
                 data_fields[field] = vector_field(data_arrays[j])
             if isinstance(fe_data.data_fields[field],scalar_field):
                 data_fields[field] = scalar_field(data_arrays[j])
+            if isinstance(fe_data.data_fields[field],symmetric_rank_two_field):
+                data_fields[field] = symmetric_rank_two_field(data_arrays[j])
 
         new_metadata = fe_data.metadata
 
