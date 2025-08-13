1e29802f52357a170ee82510902d5e78afcbbf63
Rory Spencer <rory.spencer@ukaea.uk>
2025-08-13 14:34:09 +0100

    Adding mpiexec functionality

--- src/mt2py/runner/config.py
+++ src/mt2py/runner/config.py
@@ -6,13 +6,14 @@ import os
 class CommandLineConfig:
 
 
-    def __init__(self,source:str,command:str,input_file:Path,output_name:str, input_tag = None):
+    def __init__(self,source:str,command:str,input_file:Path,output_name:str, input_tag = None,multiprocess = None):
 
         self.source = source
         self.command = command
         self.input_file = input_file
         self.input_tag = input_tag
         self.output_name = output_name
+        self.mp = multiprocess
 
         if not input_file.exists():
             raise FileNotFoundError('File not found at path.')
@@ -27,8 +28,11 @@ class CommandLineConfig:
         Returns:
             list: list of command line arguments
         """
-        
-        arg_list = [self.command]
+
+        if self.mp is not None:
+            arg_list = ['mpiexec','--bind-to','none','-n', '{}'.format(self.mp),self.command]
+        else:
+            arg_list = [self.command]
         if self.input_tag is not None:
             arg_list.append(self.input_tag)
         arg_list.append(str(self.input_file))
@@ -43,7 +47,7 @@ class CommandLineConfig:
         filename = self.source + '-' + str(parameter_group.id)
         output_path = output_dir / filename
         arg_list.append(self.output_name+'='+str(output_path))
-
+        #print(arg_list)
         return arg_list
 
 
