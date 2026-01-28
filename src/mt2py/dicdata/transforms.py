f4abf27c436cfa1fa7c4c2d0f0e33cc03f302f4d
Rory Spencer <rory.spencer@ukaea.uk>
2026-01-28 13:15:37 +0000

    Added bounding box

--- src/mt2py/dicdata/transforms.py
+++ src/mt2py/dicdata/transforms.py
@@ -34,3 +34,51 @@ def rot90(dicdata: DICData):
 
     return rotdata
 
+def get_by_bounding_box(dicdata: DICData,bounding_box:tuple[tuple,tuple,tuple])->DICData:
+    """ Return a copy of DICData within the bounding box
+
+    Args:
+        dicdata (DICData): _description_
+        bounding_box (tuple[tuple,tuple,tuple]): _description_
+
+    Returns:
+        DICData: _description_
+    """
+   
+
+    z_flag = dicdata.z is not None
+
+    if z_flag:
+        filter = (dicdata.x > bounding_box[0][0])*(dicdata.x < bounding_box[0][1])*(dicdata.y > bounding_box[1][0])*(dicdata.y < bounding_box[1][1])*(dicdata.z > bounding_box[2][0])*(dicdata.z < bounding_box[2][1])
+    else:
+        filter = (dicdata.x > bounding_box[0][0])*(dicdata.x < bounding_box[0][1])*(dicdata.y > bounding_box[1][0])*(dicdata.y < bounding_box[1][1])
+
+    #Need to get shape of Trues in filter
+    #print(np.max(np.where(filter)[0]))
+    y_size = 1+np.max(np.where(filter)[0])-np.min(np.where(filter)[0])
+    x_size = 1+np.max(np.where(filter)[1])-np.min(np.where(filter)[1])
+
+    trimdata = copy(dicdata)
+
+    trimdata.x = dicdata.x[filter].reshape(y_size,x_size)
+    trimdata.y = dicdata.y[filter].reshape(y_size,x_size)
+    if z_flag:
+        trimdata.z = dicdata.z[filter].reshape(y_size,x_size)
+
+    trimdata.u = dicdata.u[:,filter].reshape(-1,y_size,x_size)
+    trimdata.v = dicdata.v[:,filter].reshape(-1,y_size,x_size)
+    if z_flag:
+        trimdata.w = dicdata.w[:,filter].reshape(-1,y_size,x_size)
+
+    trimdata.mask = dicdata.mask[filter].reshape(y_size,x_size)
+
+    trimdata.exx = dicdata.exx[:,filter].reshape(-1,y_size,x_size)
+    trimdata.eyy = dicdata.eyy[:,filter].reshape(-1,y_size,x_size)
+    trimdata.exy = dicdata.exy[:,filter].reshape(-1,y_size,x_size)
+
+    return trimdata
+
+
+
+
+
